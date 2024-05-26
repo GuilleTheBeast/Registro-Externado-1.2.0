95cd0c52afec31afd6dd7fddd0ccfbbe7caf31a2
GuilleTheBeast <00026018@uca.edu.sv>
2024-05-26 09:08:43 -0600

    Adding UI changes

--- Frontend/src/componentes/Asistentes/Verinformacionresponsables.js
+++ Frontend/src/componentes/Asistentes/Verinformacionresponsables.js
@@ -73,9 +73,9 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
       } else if (userRole === 3 || userRole === "3") {
         //console.log("Entraste al if de rol 3");
         navigate("/negado");
-      }else if (userRole === 4 || userRole === "4") {
+      } else if (userRole === 4 || userRole === "4") {
         //console.log("Entraste al if de rol 4");
-        
+
       } else {
         //console.log("El rol es " + payloadJson.rol);
       }
@@ -234,13 +234,11 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
             {/* Tercera fila */}
             <Row className="mb-3">
               <Col md={6}>
-                <Form.Group controlId="direccionResidencia">
-                  <Form.Label style={{ paddingTop: "10px" }}>
-                    Dirección de residencia:
-                  </Form.Label>
+                <Form.Group controlId="direccionResidencia" style={{ marginTop: '10px' }}>
+                  <Form.Label>Correo electrónico:</Form.Label>
                   <Form.Control
                     type="text"
-                    defaultValue={responsable.externado_address}
+                    defaultValue={responsable.externado_email}
                     readOnly
                   />
                 </Form.Group>
@@ -266,7 +264,7 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
                   </Form.Label>
                   {departamento.map((depto) =>
                     depto.idexternado_departments ===
-                    responsable.externado_department_id ? (
+                      responsable.externado_department_id ? (
                       <Form.Control
                         type="text"
                         defaultValue={depto.externado_department}
@@ -278,44 +276,7 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
               </Col>
             </Row>
             {/* Cuarta fila */}
-            <Row className="mb-3">
-              <Col md={3}>
-                <Form.Group controlId="municipio">
-                  <Form.Label id="fechaNacimiento-label">
-                    Fecha de nacimiento:
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={fechaNacimiento}
-                    disabled
-                  >
-                    {/* Opciones del select */}
-                  </Form.Control>
-                </Form.Group>
-              </Col>
-              <Col md={3}>
-                <Form.Group controlId="nacionalidad">
-                  <Form.Label id="nacionalidad-label">Nacionalidad:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_nationality}
-                    disabled
-                  >
-                    {/* Opciones del select */}
-                  </Form.Control>
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group controlId="direccionResidencia">
-                  <Form.Label>Correo electrónico:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_email}
-                    readOnly
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
+
             {/* Quinta fila */}
             {/*Columna izquierda dividida en 2, columna derecha 1 completa*/}
             <Row className="mb-3">
@@ -323,46 +284,16 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
                 <Row>
                   <Col md={6}>
                     <Form.Group controlId="formtipodocumento">
-                      <Form.Label>Tipo de documento:</Form.Label>
+                      <Form.Label>Teléfono de trabajo:</Form.Label>
                       <Form.Control
                         type="text"
-                        defaultValue={
-                          Number(responsable.externado_id_type) === 0
-                            ? "DUI"
-                            : Number(responsable.externado_id_type) === 1
-                            ? "Pasaporte"
-                            : ""
-                        }
+                        defaultValue={responsable.externado_work_phone}
                         readOnly
                       />
                     </Form.Group>
+
                   </Col>
-                  <Col md={6}>
-                    <Form.Group controlId="formnumerodocumento">
-                      <Form.Label id="numeroDocumento-label">
-                        Número de documento:
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={responsable.externado_id}
-                        readOnly
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-              </Col>
-              <Col md={6}>
-                <Row>
-                  <Col md={6}>
-                    <Form.Group controlId="formtipodocumento">
-                      <Form.Label>Número de NIT:</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={responsable.externado_nit}
-                        readOnly
-                      />
-                    </Form.Group>
-                  </Col>
+
                   <Col md={6}>
                     <Form.Group controlId="formnumerodocumento">
                       <Form.Label id="numeroDocumento-label">
@@ -376,357 +307,6 @@ const Verinformacionresponsables = ({ setShowNavbar }) => {
                     </Form.Group>
                   </Col>
                 </Row>
-              </Col>
-            </Row>
-
-            <Row className="mb-3">
-              <Col md={6}>
-                <Form.Group controlId="formtipodocumento">
-                  <Form.Label>Profesión:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_occupation}
-                    readOnly
-                  />
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group controlId="formtipodocumento">
-                  <Form.Label>Lugar de trabajo:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_workplace}
-                    readOnly
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row className="mb-3">
-              <Col md={6}>
-                <Form.Group controlId="formtipodocumento">
-                  <Form.Label>Cargo que desempeña:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_jobposition}
-                    readOnly
-                  />
-                </Form.Group>
-              </Col>
-
-              <Col md={6}>
-                <Row>
-                  <Col md={6}>
-                    <Form.Group controlId="formtipodocumento">
-                      <Form.Label>Teléfono de trabajo:</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={responsable.externado_work_phone}
-                        readOnly
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-
-            {/* Sexta fila */}
-
-            <Row>
-              <Col md={6}>
-                <Form.Group controlId="estudio2022">
-                  <Form.Label>¿Es exalumno del Externado?:</Form.Label>
-                  <div key={`inline-radio`} className="mb-3">
-                    <Form.Check
-                      inline
-                      label="Sí"
-                      type="radio"
-                      id={`inline-radio-1`}
-                      value="Sí"
-                      checked={
-                        responsable.externado_former_externado_student === true
-                          ? true
-                          : false
-                      }
-                      disabled
-                    />
-                    <Form.Check
-                      inline
-                      label="No"
-                      type="radio"
-                      id={`inline-radio-2`}
-                      value="No"
-                      checked={
-                        responsable.externado_former_externado_student === false
-                          ? true
-                          : false
-                      }
-                      disabled
-                    />
-                  </div>
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group controlId="grado2023">
-                  <Form.Label>¿En qué universidad estudió?:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    defaultValue={responsable.externado_university_studies}
-                    readOnly
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col md={6}>
-                <Form.Group controlId="estudio2022">
-                  <Form.Label style={{ marginTop: "10px" }}>
-                    ¿Es usted actualmente una Persona Políticamente Expuesta
-                    (PEP´s)?:
-                  </Form.Label>
-                  <div key={`inline-radio`} className="mb-3">
-                    <Form.Check
-                      inline
-                      label="Sí"
-                      type="radio"
-                      id={`inline-radio-1`}
-                      value="Sí"
-                      checked={
-                        responsable.externado_pep === true ? true : false
-                      }
-                      disabled
-                    />
-                    <Form.Check
-                      inline
-                      label="No"
-                      type="radio"
-                      id={`inline-radio-2`}
-                      value="No"
-                      checked={
-                        responsable.externado_pep === false ? true : false
-                      }
-                      disabled
-                    />
-                  </div>
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group controlId="grado2023">
-                  {responsable.externado_pep === false ? (
-                    <Form.Label hidden> </Form.Label>
-                  ) : (
-                    <Form.Label style={{ marginTop: "10px" }}>
-                      Si usted es una Persona Políticamente Expuesta, por favor
-                      seleccionar según corresponda:
-                    </Form.Label>
-                  )}
-
-                  {responsable.externado_pep === false ? (
-                    <Form.Control hidden></Form.Control>
-                  ) : Number(responsable.externado_pep_occupation_id) > 0 &&
-                    Number(responsable.externado_pep_occupation_id) < 11 ? (
-                    pep.map((puesto) =>
-                      puesto.idexternado_pep ===
-                      Number(responsable.externado_pep_occupation_id) ? (
-                        <Form.Control
-                          type="text"
-                          defaultValue={puesto.externado_pep_value}
-                          readOnly
-                        />
-                      ) : null
-                    )
-                  ) : (
-                    <Form.Control type="text" defaultValue={""} readOnly />
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col></Col>
-              <Col xs={12} sm={12} md={6}>
-                <Form.Group controlId="pepOtherId">
-                  {responsable.externado_pep === false ||
-                  (Number(responsable.externado_pep_occupation_id) >= 0 &&
-                    Number(responsable.externado_pep_occupation_id) < 10) ? (
-                    <Form.Label hidden> </Form.Label>
-                  ) : (
-                    <Form.Label>
-                      Si seleccionó otro, por favor especifique:
-                    </Form.Label>
-                  )}
-                  {responsable.externado_pep === false ||
-                  (Number(responsable.externado_pep_occupation_id) >= 0 &&
-                    Number(responsable.externado_pep_occupation_id) < 10) ? (
-                    <Form.Control hidden></Form.Control>
-                  ) : Number(responsable.externado_pep_occupation_id) === 10 ? (
-                    <Form.Control
-                      type="text"
-                      defaultValue={responsable.externado_pep_occupation_other}
-                      readOnly
-                    />
-                  ) : (
-                    <Form.Control type="text" defaultValue={""} readOnly />
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={6}>
-                <Form.Group controlId="estudio2022">
-                  <Form.Label style={{ marginTop: "20px" }}>
-                    ¿Ha sido usted una Persona Políticamente Expuesta (PEP´s)?
-                    en los últimos 3 años?:
-                  </Form.Label>
-                  <div key={`inline-radio`} className="mb-3">
-                    <Form.Check
-                      inline
-                      label="Sí"
-                      type="radio"
-                      id={`inline-radio-1`}
-                      value="Sí"
-                      checked={
-                        responsable.externado_pep_3years === true ? true : false
-                      }
-                      disabled
-                    />
-                    <Form.Check
-                      inline
-                      label="No"
-                      type="radio"
-                      id={`inline-radio-2`}
-                      value="No"
-                      checked={
-                        responsable.externado_pep_3years === false
-                          ? true
-                          : false
-                      }
-                      disabled
-                    />
-                  </div>
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group controlId="grado2023">
-                  {responsable.externado_pep_3years === false ? (
-                    <Form.Label hidden> </Form.Label>
-                  ) : (
-                    <Form.Label style={{ marginTop: "20px" }}>
-                      Si usted es una Persona Políticamente Expuesta, por favor
-                      seleccionar según corresponda:
-                    </Form.Label>
-                  )}
-
-                  {responsable.externado_pep_3years === false ? (
-                    <Form.Control hidden></Form.Control>
-                  ) : Number(responsable.externado_pep_3years_occupation_id) >
-                      0 &&
-                    Number(responsable.externado_pep_3years_occupation_id) <
-                      11 ? (
-                    pep.map((puesto) =>
-                      puesto.idexternado_pep ===
-                      Number(responsable.externado_pep_3years_occupation_id) ? (
-                        <Form.Control
-                          type="text"
-                          defaultValue={puesto.externado_pep_value}
-                          readOnly
-                        />
-                      ) : null
-                    )
-                  ) : (
-                    <Form.Control type="text" defaultValue={""} readOnly />
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col></Col>
-              <Col xs={12} sm={12} md={6}>
-                <Form.Group controlId="pepOtherId">
-                  {responsable.externado_pep_3years === false ||
-                  (Number(responsable.externado_pep_3years_occupation_id) >=
-                    0 &&
-                    Number(responsable.externado_pep_3years_occupation_id) <
-                      10) ? (
-                    <Form.Label hidden> </Form.Label>
-                  ) : (
-                    <Form.Label>
-                      Si seleccionó otro, por favor especifique:
-                    </Form.Label>
-                  )}
-                  {responsable.externado_pep_3years === false ||
-                  (Number(responsable.externado_pep_3years_occupation_id) >=
-                    0 &&
-                    Number(responsable.externado_pep_3years_occupation_id) <
-                      10) ? (
-                    <Form.Control hidden></Form.Control>
-                  ) : Number(responsable.externado_pep_3years_occupation_id) ===
-                    10 ? (
-                    <Form.Control
-                      style={{ marginBottom: "20px" }}
-                      type="text"
-                      defaultValue={
-                        responsable.externado_pep_3years_occupation_other
-                      }
-                      readOnly
-                    />
-                  ) : (
-                    <Form.Control type="text" defaultValue={""} readOnly />
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
-            {/* Septima fila */}
-
-            {/* Octava fila */}
-            <Row>
-              <Col md={6}>
-                <Form.Group>
-                  <Form.Label style={{ paddingTop: "10px" }}>
-                    Rango promedio de sus ingresos mensuales familiares (acá
-                    incluye su salario, recepción de remesas, pensiones,
-                    ganancias de su negocio, etc.) Si su rango es superior a
-                    $3,000 dólares mensuales, por favor especifique un monto
-                    aproximado
-                  </Form.Label>
-                  {incoming.map((salario) =>
-                    salario.idexternado_incomings ===
-                    Number(responsable.externado_incomings_id) ? (
-                      <Form.Control
-                        type="text"
-                        defaultValue={salario.externado_incomings_value}
-                        readOnly
-                      />
-                    ) : null
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
-
-            {/* Decima quinta fila */}
-            <Row>
-              <Col md={6}>
-                <Form.Group controlId="nombres">
-                  {Number(responsable.externado_incomings_id) === 7 ? (
-                    <Form.Label style={{ marginTop: "20px" }}>
-                      Responsable:
-                    </Form.Label>
-                  ) : (
-                    <Form.Label
-                      hidden
-                      style={{ marginTop: "20px" }}
-                    ></Form.Label>
-                  )}
-
-                  {Number(responsable.externado_incomings_id) === 7 ? (
-                    <Form.Control
-                      style={{ paddingBottom: "10px" }}
-                      type="text"
-                      defaultValue={responsable.externado_incomings_other}
-                      readOnly
-                    />
-                  ) : null}
-                </Form.Group>
               </Col>
             </Row>
 
